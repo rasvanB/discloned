5d04f0e7ac2c59472637a24ba0607aba3ce153b0
rasvanB <not.obelix@gmail.com>
2023-08-22 23:11:00 +0300

    set up drizzle-kit scripts

---
+++ drizzle.config.ts
@@ -0,0 +1,18 @@
+import * as dotenv from "dotenv";
+
+dotenv.config({
+  path: ".env.local",
+});
+
+import type { Config } from "drizzle-kit";
+
+export default {
+  schema: "./src/db/schema.ts",
+  driver: "turso",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+    authToken: process.env.DATABASE_AUTH_TOKEN,
+  },
+  verbose: true,
+  strict: true,
+} satisfies Config;
