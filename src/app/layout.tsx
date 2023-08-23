d707d51f4627b7aa85c7b584c70775144517e24d
rasvanB <not.obelix@gmail.com>
2023-08-23 22:41:51 +0300

    session provider wrapper for client components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,20 @@
+import NextAuthSessionProvider from "@/components/session-provider";
 import "./globals.css";
+import { getServerAuthSession } from "@/lib/auth";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerAuthSession();
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className="dark bg-background">
+      <body>
+        <NextAuthSessionProvider session={session}>
+          {children}
+        </NextAuthSessionProvider>
+      </body>
     </html>
   );
 }
