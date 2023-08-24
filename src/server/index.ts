4a728d0d15c1d7461817a9242f4557e61444a105
rasvanB <not.obelix@gmail.com>
2023-08-24 21:40:19 +0300

    moved register logic from api route to trpc

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,62 @@
+import { z } from "zod";
+import bcrypt from "bcrypt";
 import { publicProcedure, router } from "./trpc";
+import { db } from "@/db";
+import { users } from "@/db/schema";
+import { eq } from "drizzle-orm";
+import { TRPCError } from "@trpc/server";
 
 export const appRouter = router({
-  getTest: publicProcedure.query(async () => {
-    return [10, 20, 30];
-  }),
+  register: publicProcedure
+    .input(
+      z.object({
+        name: z.string().min(3).max(30),
+        email: z.string().email(),
+        password: z.string().min(8).max(100),
+      })
+    )
+    .mutation(async ({ input }) => {
+      const { name, email, password } = input;
+      const hashedPassword = await bcrypt.hash(password, 12);
+
+      try {
+        await db
+          .insert(users)
+          .values({
+            name,
+            email,
+            hashedPassword,
+          })
+          .execute();
+      } catch (error) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Could not create user",
+        });
+      }
+
+      const dbResult = await db
+        .select({
+          id: users.id,
+          name: users.name,
+          email: users.email,
+          image: users.image,
+        })
+        .from(users)
+        .where(eq(users.email, email))
+        .execute();
+
+      const user = dbResult[0];
+
+      if (!user) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Something went wrong",
+        });
+      }
+
+      return user;
+    }),
 });
 
 export type AppRouter = typeof appRouter;
