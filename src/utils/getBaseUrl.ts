60ecaa3415d8f10c082c5840a1ca2571987e5ccf
rasvanB <not.obelix@gmail.com>
2023-08-26 21:31:06 +0300

    get base url util function

---
+++ src/utils/getBaseUrl.ts
@@ -0,0 +1,5 @@
+export default function getBaseUrl() {
+  if (typeof window !== "undefined") return "";
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+  return `http://localhost:${process.env.PORT ?? 3000}`;
+}
