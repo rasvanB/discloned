a94525eb112fa50fe453b21709bef89af4c345ae
rasvanB <not.obelix@gmail.com>
2023-08-26 21:32:05 +0300

    redirecting to auth if no session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import { getServerAuthSession } from "@/lib/auth";
+import { redirect } from "next/navigation";
+
 export default async function Home() {
-  return <main></main>;
+  const session = await getServerAuthSession();
+  if (!session) redirect("/auth/login");
+  redirect("/home");
+
+  return (
+    <main className="flex flex-col w-screen h-screen">
+      <div>SERVER: {JSON.stringify(session)}</div>
+    </main>
+  );
 }
