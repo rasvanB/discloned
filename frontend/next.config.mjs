397263b50ea711565f813cdf7529b6565045babe
rasvanB <not.obelix@gmail.com>
2023-09-12 23:09:56 +0300

    modify next config to fix socketio warnings

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,14 @@
 import "./src/env.mjs";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    config.externals.push({
+      "utf-8-validate": "commonjs utf-8-validate",
+      bufferutil: "commonjs bufferutil"
+    });
+
+    return config;
+  },
   images: {
     remotePatterns: [
       {
