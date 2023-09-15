a300d42924fde87c0cd1ed8ae9a375fbb58ded8c
rasvanB <not.obelix@gmail.com>
2023-09-15 23:21:27 +0300

    add default server icon

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,2 +1,5 @@
 export const DEFAULT_USER_IMAGE_SRC =
   "https://uploadthing.com/f/95db80ec-73a2-484f-898d-238b5424ce70_5770f01a32c3c53e90ecda61483ccb08.jpg";
+
+export const DEFAULT_GUILD_IMAGE_SRC =
+  "https://utfs.io/f/b1902183-c5b0-489f-b2e9-826830de1924-medn8j.jpg";
