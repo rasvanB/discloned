f4beac90e523f35a4b4f8b6a782911ca98865497
rasvanB <not.obelix@gmail.com>
2023-09-18 23:09:28 +0300

    clean up message list component

--- frontend/src/components/message-list.tsx
+++ frontend/src/components/message-list.tsx
@@ -6,7 +6,6 @@ import Message from "@/components/message";
 import useSocket from "@/hooks/use-socket";
 import { useEffect, useRef } from "react";
 import { Button } from "@/components/ui/button";
-import { useInView } from "react-intersection-observer";
 
 const MessageList = ({
   initialData,
@@ -18,20 +17,23 @@ const MessageList = ({
   memberId: string;
 }) => {
   const ref = useRef<HTMLDivElement>(null);
-  const { ref: inViewRef, inView } = useInView({
-    threshold: 1,
-    delay: 500,
-    rootMargin: "500px",
-  });
 
-  const { isConnected } = useSocket({ channelId });
+  useSocket({ channelId });
 
-  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isFetching } =
     trpc.infiniteMessages.useInfiniteQuery(
       {
         channelId,
       },
       {
+        initialData: {
+          pages: [
+            {
+              items: initialData,
+            },
+          ],
+          pageParams: [null],
+        },
         refetchOnWindowFocus: false,
         refetchOnReconnect: false,
         getNextPageParam: (lastPage) => lastPage.nextCursor,
@@ -39,82 +41,41 @@ const MessageList = ({
     );
 
   useEffect(() => {
-    if (data && data.pages && data.pages.length > 1) return;
     ref?.current?.scrollIntoView({ behavior: "instant" });
     setTimeout(() => {
       ref?.current?.scrollIntoView({ behavior: "instant" });
-    }, 500);
-  }, [data, ref]);
-
-  useEffect(() => {
-    if (inView && !isFetchingNextPage && hasNextPage) {
-      fetchNextPage();
-    }
-  }, [inView, isFetchingNextPage, hasNextPage, fetchNextPage]);
+    }, 1000);
+  }, [ref]);
 
   if (!data) return null;
-
-  console.log({ inView, isFetchingNextPage, hasNextPage });
-  const allData = data.pages.map((page) => page.items).flat();
-
-  if (!allData) return null;
 
   return (
     <div className={"flex flex-col-reverse gap-2"}>
       <div className={"h-4"} ref={ref} />
-      {allData.map((message) => {
-        return (
-          <Message message={message} key={message.id} memberId={memberId} />
-        );
+      {data.pages.map((page) => {
+        return page.items.map((message) => {
+          return (
+            <Message message={message} key={message.id} memberId={memberId} />
+          );
+        });
       })}
-      {hasNextPage && <div className={"h-4"} ref={inViewRef}></div>}
+      {hasNextPage && (
+        <div className={"flex items-center justify-center"}>
+          <Button
+            className={"w-fit"}
+            variant={"secondary"}
+            size={"default"}
+            onClick={() => fetchNextPage()}
+            disabled={isFetchingNextPage || isFetching}
+          >
+            {isFetchingNextPage || isFetching
+              ? "Loading more messages..."
+              : "Load more messages"}
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
-
-// const MessageList = ({
-//   initialData,
-//   channelId,
-//   memberId,
-// }: {
-//   initialData: ProcedureOutputs["getChannelMessages"];
-//   channelId: string;
-//   memberId: string;
-// }) => {
-//   const ref = useRef<HTMLDivElement>(null);
-//
-//   const { isConnected } = useSocket({ channelId });
-//
-//   const { data } = trpc.getChannelMessages.useQuery(
-//     {
-//       channelId,
-//     },
-//     {
-//       initialData,
-//       refetchOnWindowFocus: false,
-//       refetchOnReconnect: false,
-//     },
-//   );
-//
-//   useEffect(() => {
-//     ref?.current?.scrollIntoView({ behavior: "instant" });
-//     setTimeout(() => {
-//       ref?.current?.scrollIntoView({ behavior: "instant" });
-//     }, 500);
-//   }, [data, ref]);
-//
-//   if (!data) return null;
-//
-//   return (
-//     <div className={"flex flex-col-reverse gap-2"}>
-//       <div className={"h-4"} ref={ref} />
-//       {data.map((message) => {
-//         return (
-//           <Message message={message} key={message.id} memberId={memberId} />
-//         );
-//       })}
-//     </div>
-//   );
-// };
 
 export default MessageList;
